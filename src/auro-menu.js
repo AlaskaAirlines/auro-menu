e18551edaadfac99602ce9328a88b507916a5d00
Brent Labasan <BrentLabasan@gmail.com>
2021-04-02 13:57:11 -0700

    feat: transferred dev and test files from initDev to main branch

--- src/auro-menu.js
+++ src/auro-menu.js
@@ -3,13 +3,9 @@
 
 // ---------------------------------------------------------------------
 
-// If use litElement base class
-import { LitElement, html, css } from "lit-element";
+import "@alaskaairux/auro-icon";
 
-// If using auroElement base class
-// See instructions for importing auroElement base class https://git.io/JULq4
-// import { html, css } from "lit-element";
-// import AuroElement from '@alaskaairux/webcorestylesheets/dist/auroElement/auroElement';
+import { LitElement, html, css } from "lit-element";
 
 // Import touch detection lib
 import "focus-visible/dist/focus-visible.min.js";
@@ -17,22 +13,20 @@ import styleCss from "./style-css.js";
 
 // See https://git.io/JJ6SJ for "How to document your components using JSDoc"
 /**
- * auro-menu provides users a way to ...
- *
- * @attr {String} cssClass - Applies designated CSS class to DOM element.
+ * auro-menu provides users a way to select one option from a pre-defined list of options
  */
 
-// build the component class
 class AuroMenu extends LitElement {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
 
-  // function to define props used within the scope of this component
+    this.options = null;
+  
+  }
+
   static get properties() {
     return {
-      // ...super.properties,
-      cssClass:   { type: String }
+      options: { type: Array }
     };
   }
 
@@ -42,15 +36,102 @@ class AuroMenu extends LitElement {
     `;
   }
 
-  // When using auroElement, use the following attribute and function when hiding content from screen readers.
-  // aria-hidden="${this.hideAudible(this.hiddenAudible)}"
+  // dispatchEventOptionSelected = (evt, i) => {
+  //   debugger;
+  //   // let options = this.shadowRoot.querySelector('slot[name=listOfOptions]').assignedNodes();
 
-  // function that renders the HTML and CSS into  the scope of the component
+  //   this.options[i].dispatchEvent(new CustomEvent('optionSelected', {
+  //     bubbles: true,
+  //     cancelable: false,
+  //     composed: true,
+  //     detail: {
+  //       value: this.options[i].getAttribute('data-value')
+  //     }
+  //   }));
+
+  //   alert("index" + i);
+  // }
+
+
+  firstUpdated() {
+    this.options = this.shadowRoot.querySelector('slot[name=listOfOptions]').assignedNodes();
+
+    // 1) make each option tabbable 2) add click and keypress event listeners, to dispatchEvent that a particular option
+    // was selected
+    for (let i = 0; i < this.options.length; i++) {
+      this.options[i].setAttribute('index', i)
+      this.options[i].setAttribute('tabindex', '0')
+      this.options[i].addEventListener('keydown', (evt, i) => handleKeyDown(evt, i));
+      this.options[i].addEventListener('click', (i) => handleClick(i));
+
+      let span = document.createElement('span');
+      span.id = `${i}check`;
+      span.classList.add('checkContainer')
+      // span.innerHTML = 'a&nbsp;b';
+      span.innerHTML = '<auro-icon category="interface" name="check-sm" emphasis></auro-icon>';
+
+      span.style.display = 'inline-block';
+      span.style.margin = '0 8px 0 8px';
+      span.style.padding = '0';
+
+      span.style.visibility = 'hidden';
+
+
+      // this.options[i].appendChild(span);
+      this.options[i].insertBefore(span, this.options[i].firstChild);    }
+
+
+
+    const handleKeyDown = (evt, i) => {
+      if (evt.key.toLowerCase() === 'enter' || evt.key.toLowerCase() === ' ') {
+        // this.dispatchEventOptionSelected(evt, i);
+        // debugger;
+        this.options[i.target.getAttribute('index')].dispatchEvent(new CustomEvent('optionSelected', {
+          bubbles: true,
+          cancelable: false,
+          composed: true,
+          detail: {
+            value: this.options[i].getAttribute('data-value')
+          }
+        }));
+
+      }
+    }
+    const handleClick = (i) => {
+      // this.dispatchEventOptionSelected(evt, i);
+      // debugger;
+      this.options[i.target.getAttribute('index')].dispatchEvent(new CustomEvent('optionSelected', {
+        bubbles: true,
+        cancelable: false,
+        composed: true,
+        detail: {
+          value: this.options[i.target.getAttribute('index')].getAttribute('data-value')
+        }
+      }));
+
+      let index = parseInt(i.target.getAttribute('index'));
+      // console.log("this.options[i.target.getAttribute('index')]", this.options[index]);
+// debugger;
+      for (let j = 0; j < this.options.length; j++) {
+        if (index === j) {
+          // debugger;
+          this.options[j].setAttribute('selected', '');
+          this.options[j].querySelector('span').setAttribute('style', 'display: inline-block; margin: 0 8px 0 8px; visibility: visible');
+        } else {
+          this.options[j].removeAttribute('selected');
+          this.options[j].querySelector('span').setAttribute('style', 'display: inline-block; margin: 0 8px 0 8px; visibility: hidden');
+
+        }
+      }
+  
+    }
+  }
+
   render() {
     return html`
-      <div class=${this.cssClass}>
-        <slot></slot>
-      </div>
+      <ul>
+        <slot name="listOfOptions"></slot>
+      </ul>
     `;
   }
 }
