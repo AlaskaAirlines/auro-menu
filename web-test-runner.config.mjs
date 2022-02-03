bef8d9bfcda4d7048a7f10c71fdea9a2e65a27f3
Dale Sande <dale.sande@gmail.com>
2022-02-02 22:58:50 -0800

    feat(a11y): increase interactive keyboard experience #55 #64
    This commit removes the manual complexity with keyboard experiences
    and adds support for aria roles to increase voice over experiences.
    
    Changes to be committed:
    modified:   src/auro-menu.js
    modified:   src/auro-menuoption.js
    modified:   src/style-menuoption.scss
    modified:   test/auro-menu.test.js
    modified:   web-test-runner.config.mjs

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -3,10 +3,10 @@ export default {
   nodeResolve: true,
   coverageConfig: {
     threshold: {
-      statements: 70,
-      branches: 70,
-      functions: 70,
-      lines: 70
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
     }
   }
 };
