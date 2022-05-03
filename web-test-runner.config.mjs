0541c4b1181c5345ce3273a8d3be2eadea263933
Capsule42 <jason@capsule42.com>
2022-05-02 17:28:39 -0700

    fix(value): auro-menu now handles when to call selectByValue #125

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -3,10 +3,10 @@ export default {
   nodeResolve: true,
   coverageConfig: {
     threshold: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80
+      statements: 75,
+      branches: 75,
+      functions: 75,
+      lines: 75
     }
   }
 };
