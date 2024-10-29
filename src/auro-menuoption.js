0c3d03e2dcdca59f01cbea687222834ba8e5b339
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-10-29 12:46:46 -0700

    chore: remove unused 'customSize' attribute

--- src/auro-menuoption.js
+++ src/auro-menuoption.js
@@ -117,7 +117,6 @@ export class AuroMenuOption extends LitElement {
       ${this.selected && !this.nocheckmark ? html`
         <${this.iconTag}
           ondark
-          customsize
           class="selected"
           category="interface"
           name="checkmark-sm">
